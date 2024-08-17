e5c310a326a820a9ce108e4d2c7b5aa23fc45bef
Noah Klowden <chewie@nocturne.org>
2024-08-17 14:58:21 -0700

    Added the functionality for the scene swap object

--- scripts/level_portal.gd
+++ scripts/level_portal.gd
@@ -1,4 +1,16 @@
 extends Area2D
 
+const LEVELPATH = "res://scenes/levels/"
+var levelsuffix = ".tscn"
+
+@export var scenename := ""
+
+func _ready():
+	if scenename.ends_with(".tscn"):
+		levelsuffix = ""
+
 func _on_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("Player"):
+		print("Player collided with level swapper")
+		print("Changing scene to " + scenename)
+		get_tree().change_scene_to_file.call_deferred(LEVELPATH + scenename + levelsuffix)
