84ea46e17552a17f91d5baa821eb3383a16b80ae
Mika <iombramika@gmail.com>
2024-08-18 13:57:16 +0100

    menu interface fixes

--- scripts/pause_menu_controller.gd
+++ scripts/pause_menu_controller.gd
@@ -1,17 +1,25 @@
 extends Control
 
+@onready var focus_button = $PanelContainer/VBoxContainer/ResumeButton;
+
 func resume() -> void:
 	get_tree().paused = false;
 	$AnimationPlayer.play_backwards("ScreenBlur")
+	for b : Button in $PanelContainer/VBoxContainer.get_children():
+		b.release_focus()
 	
 func pause() -> void:
 	get_tree().paused = true;
 	$AnimationPlayer.play("ScreenBlur")
+	focus_button.grab_focus()
 
 func escaping() -> void: 
-	if Input.is_action_just_pressed("pause") and !get_tree().paused:
-		pause()
-	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+	if Input.is_action_just_pressed("pause"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
+	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused:
 		resume()
 
 # Called when the node enters the scene tree for the first time.
@@ -41,7 +49,7 @@ func _on_resume_button_pressed() -> void:
 
 func _on_menu_button_pressed() -> void:
 	resume()
-	get_tree().change_scene_to_file("res://scenes/TitleScreen.tscn")
+	get_tree().change_scene_to_file("res://scenes/screens/TitleScreen.tscn")
 	pass # Replace with function body.
 
 
--- scripts/title_screen_controller.gd
+++ scripts/title_screen_controller.gd
@@ -2,16 +2,17 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$StartButton.grab_focus()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/screens/main.tscn")
 
 
 func _on_quit_button_pressed() -> void:
