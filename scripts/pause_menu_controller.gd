3dd45156564d087708f15b22aab98d92b0f561ce
RydalisDarx <allplayerof@gmail.com>
2024-08-18 17:33:29 -0700

    menu navigation and ui visibility changes
    pushed celeste's changes, along with better ui visibility for viewport.

--- scripts/pause_menu_controller.gd
+++ scripts/pause_menu_controller.gd
@@ -1,19 +1,27 @@
 extends Control
 
+@onready var focus_button = $PanelContainer/VBoxContainer/ResumeButton;
+
 func resume() -> void:
 	$PanelContainer/VBoxContainer.visible = false
 	get_tree().paused = false;
 	$AnimationPlayer.play_backwards("ScreenBlur")
+	for b : Button in $PanelContainer/VBoxContainer.get_children():
+		b.release_focus()
 	
 func pause() -> void:
 	$PanelContainer/VBoxContainer.visible = true
 	get_tree().paused = true;
 	$AnimationPlayer.play("ScreenBlur")
+	focus_button.grab_focus()
 
 func escaping() -> void: 
-	if Input.is_action_just_pressed("pause") and !get_tree().paused:
-		pause()
-	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+	if Input.is_action_just_pressed("pause"): 
+		if !get_tree().paused:
+			pause()
+		else:
+			resume()
+	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused:
 		resume()
 
 # Called when the node enters the scene tree for the first time.
