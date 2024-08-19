dcd961aff1ce8a6f45e7d4c15a0e3b6e1ce7c14a
Mika <iombramika@gmail.com>
2024-08-19 13:13:51 +0100

    Added particles for landing and wall clinging.

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -5,10 +5,10 @@ signal lost_upgrade(upgrade_name: String)
 signal upgrade_loaded(upgrade_name: String)
 
 func _ready() -> void:
-
+	pass
 	# This can be removed for release, just scales up the window when not in fullscreen. Needed to see for small pixel art resolutions
-	var window_size = Vector2i(1920, 1080)
-	DisplayServer.window_set_size(window_size)
-	DisplayServer.window_set_position(DisplayServer.screen_get_position() + (DisplayServer.screen_get_size() / 2) - ( window_size / 2) )
+	#var window_size = Vector2i(1920, 1080)
+	#DisplayServer.window_set_size(window_size)
+	#DisplayServer.window_set_position(DisplayServer.screen_get_position() + (DisplayServer.screen_get_size() / 2) - ( window_size / 2) )
 
 	# Engine.time_scale = 0.15
